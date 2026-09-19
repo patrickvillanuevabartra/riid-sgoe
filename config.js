b5b2693faf05059c520f84e0a987985c64467821
patrickvillanuevabartra <patrick.villanueva@urp.edu.pe>
2026-09-18 23:55:51 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+/* RIID — configuración. Cambiar de puente es cambiar API_URL. */
+window.RIID_CONFIG = {
+  API_URL: 'https://proxy-riid.patrick-villanueva.workers.dev/',
+  CLIENT_ID: '1070423476628-j7qemmjemlm2eoi50f0k9iohp1gbprf3.apps.googleusercontent.com',
+  DOMINIO: 'urp.edu.pe'
+};
